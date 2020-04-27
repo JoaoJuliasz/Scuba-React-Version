054855d3e18772e9970b4056eca44b6f2349230e
Joao Juliasz de Morais <joaojuliasz@Joaos-MacBook-Air.local>
2020-04-27 13:53:14 -0300

    changed app class component to a functional component

--- src/App.js
+++ src/App.js
@@ -4,23 +4,12 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pokemons: [],
-      input: '',
-    }
-  }
-  render() {
-    return(
+const App = () => (
       <div>
         <Header />
         <Home />
         <Footer />
       </div>
     )
-  }
-}
 
 export default App;
--- src/components/Center/Center.jsx
+++ src/components/Center/Center.jsx
@@ -9,26 +9,26 @@ class Center extends React.Component {
         }
     }
 
-    componentDidMount(){
-        setTimeout(this.slide2, this.state.interval)
+    componentDidMount() {
+        setTimeout(this.slide1, this.state.interval);
     }
 
     render() {
-        const { imgUrl } = this.state
+        const { imgUrl } = this.state;
         return (
             <div className="center">
                 <img src={require(`../../assets/imgs/${imgUrl}`)} alt="" />
                 <div className="text">
                     <p>
                         Fundada em 2002, A Scubatuba Operadora de Mergulho é um centro de <br /> treinamento 5 Star SDI/TDI
-                instructor,
-                formando alunos do nível básico ao <br />instrutor, além dos cursos técnicos . A operadora conta com
-                uma<br />
-                embarcação
-                para até 15 mergulhadores e tem como instrutor
-                responsável <br />Fernando Morais, nascido e criado em Fernando de Noronha, com mais de<br /> 8.000
-                mergulhos.
-            </p>
+                        instructor,
+                        formando alunos do nível básico ao <br />instrutor, além dos cursos técnicos . A operadora conta com
+                        uma<br />
+                        embarcação
+                        para até 15 mergulhadores e tem como instrutor
+                        responsável <br />Fernando Morais, nascido e criado em Fernando de Noronha, com mais de<br /> 8.000
+                        mergulhos.
+                    </p>
                 </div>
             </div>
         )
@@ -49,6 +49,33 @@ class Center extends React.Component {
             () => setTimeout(this.slide1, this.state.interval)
         )
     }
+    // slide1 = () => {
+    //     this.setState({
+    //         imgUrl: 'slide1',
+    //     },
+    //     () => setTimeout(this.slide2, this.state.interval)
+    //     )}
+    // slide2 = () => {
+    //     this.setState({
+    //         imgUrl: 'slide2',
+    //     },
+    //     () => setTimeout(this.slide3, this.state.interval)
+    //     )
+    // }
+    // slide3 = () => {
+    //     this.setState({
+    //         imgUrl: 'slide2',
+    //     },
+    //     () => setTimeout(this.slide4, this.state.interval)
+    //     )
+    // }
+    // slide4 = () => {
+    //     this.setState({
+    //         imgUrl: 'slide4',
+    //     },
+    //     () => setTimeout(this.slide1, this.state.interval)
+    //     )
+    // }
 
 };
 
