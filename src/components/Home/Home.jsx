b9d22676edb594c435abebce30d7aadbd3ede254
Joao Juliasz de Morais <joaojuliasz@Joaos-MacBook-Air.local>
2020-04-28 12:36:45 -0300

    created build folder/files

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,9 +5,11 @@ import Container from '../Container/Container'
 import ScubaPage from '../ScubaPage/ScubaPage'
 import CoursesPage from '../CoursesPage/CoursesPage'
 import PlacesPage from '../PlacesPage/PlacesPage'
+import ScrollToTop from '../ScrollTop/ScrollTop'
 
 const Home = () => (
     <div>
+    <ScrollToTop />
         <Route exact path='/' component={Container} />
         <Route path='/discover-scuba' component={ScubaPage} />
         <Route path='/cursos' component={CoursesPage} />
